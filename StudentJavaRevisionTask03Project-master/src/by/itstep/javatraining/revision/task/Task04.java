293c6ee09bf47cd0a3744a3239abe08d7a831827
User <User@User-PC>
2024-04-18 09:58:26 +0300

    Task 4 solved

--- StudentJavaRevisionTask03Project-master/src/by/itstep/javatraining/revision/task/Task04.java
+++ StudentJavaRevisionTask03Project-master/src/by/itstep/javatraining/revision/task/Task04.java
@@ -30,6 +30,11 @@ package by.itstep.javatraining.revision.task;
 
 public class Task04 {
     public static boolean start(int x1, int y1, int x2, int y2) {
-        return false;
+    	if (x1 < 1 || x1 > 8 || x2 < 1 || x2 > 8
+	    		|| y1 < 1 || y1 > 8 || y2 < 1 || y2 > 8 
+	    		||  (x1 == x2 && y1 == y2)) {
+	        return false; 
+	    }
+    	return Math.abs(x2 - x1) <= 1 && Math.abs(y2 - y1) <= 1;
     }
 }
